449c1abcf6d8bae6bcedf134892a1eb29bba01cb
traumapoint7 <traumapoint7@gmail.com>
2025-04-07 11:02:45 +0900

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+let map, geocoder, places;
+
+window.onload = function () {
+  map = new kakao.maps.Map(document.getElementById('map'), {
+    center: new kakao.maps.LatLng(37.5665, 126.978),
+    level: 7
+  });
+
+  geocoder = new kakao.maps.services.Geocoder();
+  places = new kakao.maps.services.Places();
+
+  document.getElementById('searchBtn').addEventListener('click', () => {
+    findTraumapoint();
+  });
+};
+
 const destinations = [
   { name: "인천성모병원", x: "126.678", y: "37.453" },
   { name: "송도소방서", x: "126.644", y: "37.390" },
@@ -7,7 +23,7 @@ const destinations = [
   { name: "김포소방서", x: "126.718", y: "37.616" }
 ];
 
-function findTraumapoint(places, geocoder, map) {
+function findTraumapoint() {
   const keyword = document.getElementById('startInput').value;
 
   places.keywordSearch(keyword, (data, status) => {
