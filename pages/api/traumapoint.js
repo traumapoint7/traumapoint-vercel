db0dcc39e3c7a1081d695bc4b2dfcce775720cff
traumateam <traumapoint7@gmail.com>
2025-06-04 13:08:57 +0900

    ba

--- pages/api/traumapoint.js
+++ pages/api/traumapoint.js
@@ -1,3 +1,9 @@
+const traumaPoints = require("../../../public/data/traumaPoints_within_9km.json");
+
 export default function handler(req, res) {
-  return res.status(200).json({ message: "✅ API 정상 작동 중" });
+  console.log("✅ API 호출됨");
+  res.status(200).json({
+    message: "✅ API 정상 작동 중",
+    count: traumaPoints.length,
+  });
 }
